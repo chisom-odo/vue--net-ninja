bbd88eacbad43b9a5711f1758e0660b8da7a2576
Chisom Odo <odochisomesther@gmail.com>
2023-01-21 10:41:21 +0100

    Create Parent.Vue
    Created Parent.vue to practice props with techcheck

---
+++ src/components/Parent.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- When you are trying to pass in individual data from parent to child component -->
+  <!--<Album :artist="album.artist" :album="album.album" /> -->
+<Album v-bind="album"/>
+</template>
+<script>
+import Album from './Album.vue'
+    export default{
+        name:"Parent",
+        data(){
+            return{
+              // rawDate: new Date(1986 ,2, 3),
+              album:{
+                artist:"Chisom",
+                album:"Master of puppets",
+                albumNumber:"3",
+                rawDate: new Date(1986 ,2, 3),
+                goodAlbum:false
+              }  
+            }
+
+        },
+        methods:{
+            
+        },
+        components:{
+          Album  
+        }
+    }
+</script>
